dde41a9fecc606a409abf152a190481b65515603
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-20 12:53:07 +0530

    Add files via upload

---
+++ SIMPLE PROGRAMS/example.java
@@ -0,0 +1,10 @@
+public class example
+{
+    public static void main(String args[])
+    {
+        float x=3.4f;
+        int y=(int)x;
+        System.out.print("\nValue of x "+x);
+        System.out.print("\nValue of y "+y);
+    }
+}
