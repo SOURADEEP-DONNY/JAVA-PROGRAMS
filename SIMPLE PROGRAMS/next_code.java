49cd5ff4c6c7a58e830916155f31d810382241dd
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-09-04 15:50:39 +0530

    Add files via upload

---
+++ SIMPLE PROGRAMS/next_code.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class next_code
+{
+    public static void main(String args[])
+    {
+        String city;
+        Scanner obj=new Scanner(System.in);
+        System.out.print("\nEnter your city:-");
+        city=obj.next();
+        System.out.print("\n"+city);
+    }
+}
