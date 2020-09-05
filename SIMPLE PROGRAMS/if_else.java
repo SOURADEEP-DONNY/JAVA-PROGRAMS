e5d0dfac537c992558c0b1e3ed2f4cfd5b426914
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-09-05 21:40:48 +0530

    Add files via upload

---
+++ SIMPLE PROGRAMS/if_else.java
@@ -0,0 +1,15 @@
+class if_else
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        if (a>=60)
+        {
+            System.out.print("Senior Citizen");
+        }
+        else
+        {
+            System.out.print("Not a senior citizen");
+        }
+    }
+}
