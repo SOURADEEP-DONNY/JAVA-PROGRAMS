e8df49cdd22e6d1372a75572cd6c6ff3c378796f
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-09-05 22:45:50 +0530

    Add files via upload

---
+++ SIMPLE PROGRAMS/switch_case.java
@@ -0,0 +1,22 @@
+class switch_case
+{
+    public static void main(String args[])
+    {
+        int a=2;
+        switch(a)
+        {
+            case 1:{
+                    System.out.print("1st option");
+                    break;
+                }
+            case 2:{
+                    System.out.print("2nd option");
+                    break;
+                }
+            default:{
+                        System.out.print("INVALID OPTION");
+                    }
+        }
+    }
+}
+                
