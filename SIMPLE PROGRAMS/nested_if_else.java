7ddbcdf3de489f44170d41dffc3e8b5e16f9daf6
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-09-05 21:50:21 +0530

    Add files via upload

---
+++ SIMPLE PROGRAMS/nested_if_else.java
@@ -0,0 +1,18 @@
+class nested_if_else
+{
+    public static void main(String args[])
+    {
+        int a=90;
+        if (a>=60)
+        {
+            if(a>=90)
+            {
+                System.out.print("FULL PENSION");
+            }
+        }
+        else
+        {
+            System.out.print("NO PENSION");
+        }
+    }
+}
