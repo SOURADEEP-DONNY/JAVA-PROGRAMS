0846fb8357ee3c8e35a881b668f36a1c82ba41cf
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-08-26 19:08:18 +0530

    Add files via upload

---
+++ Java Codes/class LOGICAL_OPERATORS.java
@@ -0,0 +1,11 @@
+class LOGICAL_OPERATORS
+{
+    public static void main(String args[])
+    {
+        int a=5,x=20;
+        int b=10,y=30;
+        boolean c;
+        c=a>x && y>b;
+        System.out.println("RESULT = "+c);
+    }
+}
