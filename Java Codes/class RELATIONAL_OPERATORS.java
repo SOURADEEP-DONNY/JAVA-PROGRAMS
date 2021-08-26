36a8876058631b3d85a24e8d43eaf3aa584f5c1c
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-08-26 18:47:47 +0530

    Add files via upload

---
+++ Java Codes/class RELATIONAL_OPERATORS.java
@@ -0,0 +1,11 @@
+class RELATIONAL_OPERATORS
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=10;
+        boolean c;
+        c=a>b;
+        System.out.println("Result = "+c);
+    }
+}
